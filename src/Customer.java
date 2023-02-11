398667ce34d02ab451455c3dcc8920e1c2716d8b
harika234 <harikacdm@gmail.com>
2023-02-11 17:23:14 +0530

    213

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,32 @@
 public class Customer {
+    private String name;
+    private int creditLimit;
+    private String email;
 
+
+    public Customer(){
+        this("Nobody",1000,"nobody@nowhere.com");
+    }
+
+    public Customer(String name,String email){
+
+        this(name,1000,email);
+    }
+    public Customer(String name, int creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Customer customer = new Customer("Harry",1000,"harika@gmail.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
+
+        Customer thirdCustomer = new Customer("joe",1250,"joe@gmail.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail());
+
     }
 }
